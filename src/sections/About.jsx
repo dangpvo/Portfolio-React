4b83c7e5c409a44e5711b5f16d54374c93442964
Dennis Vo <dennisphuc@gmail.com>
2025-04-02 19:41:09 +0200

    fix: update about section

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -7,33 +7,37 @@ const About = () => {
       <section id="about" className="about container section">
         <div className="about__content">
           <h2 className="about__title">About</h2>
-          <p className="about__description">
-            Hello there! I'm Dang, a <strong>Frontend / Fullstack</strong> web
-            developer based near Montreal. I wake up in the morning, grab a cup
-            of timmies coffee, open up my laptop and build things that live on
-            the web all day, everyday.
+          <p class="about__description">
+            Hello! I'm Dang, a <strong>Frontend / Fullstack</strong> web
+            developer based in Germany. Every day, I immerse myself in building
+            scalable and efficient web solutions, driven by a passion for clean,
+            intuitive design.
           </p>
-          <p className="about__description">
-            People who know me say I'm obsessed, that's fair, I am very driven
-            and love learning new things.
+          <p class="about__description">
+            I am deeply committed to continuous learning and improving my
+            skills, always striving to tackle new challenges and create
+            meaningful, impactful projects.
           </p>
-          <p className="about__description">
-            I recently created a YouTube channel called
-            <a>Slaying The Dragon</a> where I upload videos on front-end web
-            development topics.
+          <p class="about__description">
+            I am open to collaborating on meaningful projects involving React,
+            JavaScript, and .NET development.
           </p>
           <hr className="about__hr" />
-          <h3 className="about__subtitle">Technologies</h3>
-          <div className="about__ul-container">
-            <ul className="about__ul">
-              <li className="about__list">JavaScript (ES6+)</li>
-              <li className="about__list">CSS</li>
-              <li className="about__list">HTML</li>
+          <h3 class="about__subtitle">Connect with me</h3>
+          <div class="about__ul-container">
+            <ul class="about__ul">
+              <li class="about__list">
+                <a href="https://www.linkedin.com/in/dangvo95/" target="_blank">
+                  <i class="bx bxl-linkedin-square"></i>LinkedIn
+                </a>
+              </li>
             </ul>
-            <ul className="about__ul">
-              <li className="about__list">ReactJS</li>
-              <li className="about__list">NextJS</li>
-              <li className="about__list">TailwindCSS</li>
+            <ul class="about__ul">
+              <li class="about__list">
+                <a href="https://github.com/dangpvo" target="_blank">
+                  <i class="bx bxl-github"></i>GitHub
+                </a>
+              </li>
             </ul>
           </div>
         </div>
