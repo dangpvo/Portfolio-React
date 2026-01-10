9276c61b40777431ecb19f8eafc0d414731ac743
Dennis Vo <dennisphuc@gmail.com>
2026-01-10 19:33:37 +0100

    feat: added new images to assets

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -17,8 +17,11 @@ import elysianStayImg from "./ElysianStay/elysianhotel.png";
 import autoDefenderImg from "./Others/autodefender_1.png";
 
 // Static assets
-import avatar_about from "./avatarAbout_1.jpg";
 import avatar_home from "./avatarHome_1.jpg";
+import logo_home from "./personal-logo.png";
+
+import about_dark_image from "./about-dark-1.png";
+import about_light_image from "./about-light-1.png";
 
 // Techstack assets
 import fe_angular from "./TechStack/Frontend/angular.png";
@@ -43,8 +46,10 @@ import db_postgres from "./TechStack/Database/postgres.png";
 import db_mysql from "./TechStack/Database/mysql.png";
 
 export const assets = {
-  avatar_about,
   avatar_home,
+  logo_home,
+  about_dark_image,
+  about_light_image,
 };
 
 // Sample Project Item
@@ -64,17 +69,23 @@ export const projects = [
   {
     _id: "fugina",
     name: "Fugina Shop",
-    description: `FuginaShop is an intuitive e-commerce platform offering a seamless
+    description: `FuginaShop is an intuitive e-commerce platform prototype offering a seamless
           shopping experience with a clean design, fast performance, and
           user-friendly features. It is built to provide an easy way for users
           to browse and purchase products online.`,
-    description_1: `FuginaShop is a modern, responsive e-commerce platform designed
+    description_1: `FuginaShop is a modern, responsive e-commerce platform prototype designed
               for a seamless shopping experience.`,
     description_2: ` With a clean, minimalist design and intuitive interface,
               FuginaShop delivers fast load times and smooth interactions across
               all devices. Whether shopping for personal items or exploring new
               trends, it offers a hassle-free and enjoyable online experience.`,
-    tech_stack: ["ReactJS", "TailwindCSS", "Context API", "React Router"],
+    tech_stack: [
+      "React",
+      "Vite Build Tool",
+      "TailwindCSS",
+      "Context API",
+      "React Router",
+    ],
     features: [
       "🛍️ Product listing with categories & subcategories",
       "🛒 Shopping cart management",
@@ -98,17 +109,20 @@ export const projects = [
   {
     _id: "elysian",
     name: "Elysian Stay",
-    description: `FuginaShop is an intuitive e-commerce platform offering a seamless
-          shopping experience with a clean design, fast performance, and
-          user-friendly features. It is built to provide an easy way for users
-          to browse and purchase products online.`,
-    description_1: `ElysianStay is a modern, responsive hotel management platform designed 
+    description: ``,
+    description_1: `ElysianStay is a modern, responsive hotel management platform prototype designed 
                 to simplify room booking and hotel operations.`,
     description_2: `With an intuitive interface and elegant design, ElysianStay allows users 
                 to easily browse rooms, make reservations, and manage hotel operations 
                 seamlessly across all devices. Whether for guests or hotel staff, it offers 
                 a smooth and efficient experience.`,
-    tech_stack: ["ReactJS", "TailwindCSS", "Context API", "React Router"],
+    tech_stack: [
+      "React",
+      "Vite Build Tool",
+      "TailwindCSS",
+      "Context API",
+      "React Router",
+    ],
     features: [
       "🏨 Browse available rooms with details and images",
       "📅 Room booking and reservation management",
@@ -124,9 +138,16 @@ export const projects = [
     _id: "uplinx",
     name: "Uplinx Dashboard",
     description: ``,
-    description_1: `Uplinx is a responsive React admin dashboard built with modern technologies like Vite, Tailwind CSS, and Recharts.`,
-    description_2: `The dashboard includes dynamic charts, and modular components designed for easy extension. Uplinx is ideal for developers building admin panels, analytics tools, or SaaS platforms. Features like sidebar navigation, and reusable card components make development faster.`,
-    tech_stack: ["ReactJS"],
+    description_1: `Uplinx is a responsive admin dashboard prototype designed to provide a clean, intuitive interface for managing data and visualizing insights in modern web applications.`,
+    description_2: `The dashboard includes dynamic charts, and modular components designed for easy extension. Uplinx is ideal for developers building admin panels, analytics tools, or SaaS platforms.`,
+    tech_stack: [
+      "React",
+      "Vite Build Tool",
+      "TailwindCSS",
+      "Context API",
+      "React Router",
+      "Recharts",
+    ],
     features: [],
     url: "https://uplinx.netlify.app/",
     paper: "",
@@ -137,32 +158,30 @@ export const projects = [
     name: "AutoDefender Dashboard",
     description: ``,
     description_1: `AutoDefender is a responsive React dashboard that visualizes real-time global cyberattacks and in-car component security across multiple automotive fleets.`,
-    description_2: `Built with React, Recharts, DeckGL, Maplibre, and React Globe, it delivers immersive 2D and 3D attack maps and interactive fleet dashboards. With Supabase authentication and modular components, AutoDefender is ideal for cybersecurity, automotive monitoring, and IoT visualization projects.`,
-    tech_stack: ["ReactJS"],
+    description_2: `It delivers immersive 2D and 3D attack maps, interactive fleet dashboards, and real-time security insights. With authentication and a modular architecture, AutoDefender is well suited for cybersecurity, automotive monitoring, and IoT visualization projects.`,
+    tech_stack: [
+      "React",
+      "Vite Build Tool",
+      "Context API",
+      "React Router",
+      "Recharts",
+      "DeckGL",
+      "Maplibre",
+      "React Globe",
+      "Supabase",
+    ],
     features: [],
     url: "https://autodefender.netlify.app/",
     paper: "",
     images: [autoDefenderImg],
   },
-  // {
-  //   _id: "portfolio",
-  //   name: "Portfolio",
-  //   description: ``,
-  //   description_1: `My portfolio highlights my skills in front-end development, featuring projects that blend functionality with clean, modern design.`,
-  //   description_2: `Designed to be lightweight and responsive, it includes sections for my work, skills, and contact information. This project reflects my ability to build clean, structured, and easy-to-navigate web applications.`,
-  //   tech_stack: ["ReactJS"],
-  //   features: [],
-  //   url: "https://dangpvo.netlify.app/",
-  //   paper: "",
-  //   images: [portfolioImg],
-  // },
   {
     _id: "tuc_swc",
     name: "TUC_SWC_Designer",
     description: ``,
     description_1: `A block-based coding interface designed to simplify programming for automotive and drone systems.`,
     description_2: `It enables users to visually create and execute commands, making complex functionalities more accessible. Improves task completion time by 67% and enhances engagement through an interactive and flexible design. Supports hands-on learning with dynamic functions for real-world applications.`,
-    tech_stack: [],
+    tech_stack: ["Angular", "Node.js", "ASP.Net Core", "PostgreSQL", "C++"],
     features: [],
     url: "https://youtu.be/Hke_8DwVb-U?si=9QK3thUK2SA1dR2S",
     paper: "https://ieeexplore.ieee.org/document/10690658",
@@ -174,7 +193,7 @@ export const projects = [
     description: ``,
     description_1: `A Revit plugin designed to simplify data import/export processes between Revit and external platforms.`,
     description_2: `It enables users to quickly transfer large sets of data, improving workflow efficiency in architectural and engineering projects. Achieved over 1000 downloads with excellent user feedback, making it a valuable tool for professionals in the AEC industry.`,
-    tech_stack: [],
+    tech_stack: ["C#", "WPF", "RevitAPI", "EPPlus", "Syncfusion UI"],
     features: [],
     url: "https://youtu.be/h4Mu-IUzuEg?si=3b5TO8hD-huhA2W1",
     paper: "",
@@ -186,7 +205,7 @@ export const projects = [
     description: ``,
     description_1: `PEGO is a genetic algorithm-based application developed to optimize the timetabling process in the Vietnamese education system.`,
     description_2: `By automating the generation of schedules, it significantly reduced the time required for creating timetables, making the process faster and more efficient. With an impressive 98% reduction in generation time, PEGO cut down the scheduling process from one week to just 22 minutes for over 70 teachers and 1500 students.`,
-    tech_stack: [],
+    tech_stack: ["C#", "WPF", "SQL Server", "EPPlus"],
     features: [],
     url: "https://youtu.be/hEej9yV276U?si=-YBUGBpdUxN8Yza3",
     paper: "",
